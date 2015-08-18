611b97255e1f7fabc5bcbb54b9bb2955b7098365
Nicht Jens <nichtjens@gmail.com>
2015-08-19 00:07:26 +0200

    widechar replacement via fake_wchar() now covers a wider range of letters

--- fakewchars.cpp
+++ fakewchars.cpp
@@ -1,18 +1,31 @@
 #include <string>
+#include <cstring>
 #include "fakewchars.h"
+
+const char mapping[] = 
+//	"ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ"
+	"AAAAAAACEEEEIIIIDNOOOOOx0UUUUYPsaaaaaaaceeeeiiiidnooooo:0uuuuypy";
+
 
 std::string fake_wchar(int wc_closing_key)
 {
+	wc_closing_key -= 1<<7;
+	std::string result = "_";
+	if (wc_closing_key < strlen(mapping))
+		result = std::string(1, mapping[wc_closing_key]);
+
 	switch (wc_closing_key)
 	{
-		case 159: return "ss";
-		case 164: return "ae";
-		case 182: return "oe";
-		case 188: return "ue";
-		case 132: return "Ae";
-		case 150: return "Oe";
-		case 156: return "Ue";
-		default:  return "";
+		case 31: return "ss";  // ß
+		case 36: return "ae";  // ä
+		case 54: return "oe";  // ö
+		case 60: return "ue";  // ü
+		case  4: return "Ae";  // Ä
+		case 22: return "Oe";  // Ö
+		case 28: return "Ue";  // Ü
+		case 38: return "ae";  // æ
+		case  6: return "AE";  // Æ
+		default: return result;
 	}
 }
 
